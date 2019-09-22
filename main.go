aa4b0cdb69eb5a302d03d27996c2a61e7fd36231
Ala <alaharirchi@gmail.com>
2019-09-22 21:55:18 +0200

    add basic PoC
    - serve on 8080
    - add Study type
    - refactor

--- main.go
+++ main.go
@@ -1,180 +1,265 @@
-// Ala Harirchi            July 2019
-// credit - go-graphql hello world example
-// tutorial: https://tutorialedge.net/golang/go-graphql-beginners-tutorial/
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
+	"net/http"
 
-    "github.com/graphql-go/graphql"
+	//"encoding/json"
+	//"fmt"
+	"log"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
+type Study struct {
+	ID          int
+	Title       string
+	Description string
+	Surveys     []Survey
+}
+
 type Survey struct {
-    ID int
-    Title string
-    Description string
-    Questions[] Question
+	ID          int
+	Title       string
+	Description string
+	Questions   []Question
+	StudyId     int // TODO: is the connection many-to-many?
 }
 
 type Question struct {
-    ID int
-    Text string
-    SurveyId int // TODO: make it a reference to an instanse of Survey 
-    Answers[] string
+	ID       int
+	Text     string
+	SurveyId int // TODO: make it a reference to an instanse of Survey
+	Answers  []string
 }
 
-func populate() Survey {
-    survey1: = Survey {
-        ID: 1,
-        Title: "Sample Survey",
-        Description: "Just a dummy survey",
-        Questions: [] Question {}
-    }
+var study1 Study
 
-        question1: = Question {
-        ID: 1,
-        Text: "Have you had any of these symptoms?",
-        SurveyId: survey1.ID,
-        Answers: [] string {
-            "Fever", "Chills", "Sneezing", "Cough",
-        },
-    }
-    question2: = Question {
-        ID: 2,
-        Text: "Have you visited a doctor?",
-        SurveyId: survey1.ID,
-        Answers: [] string {
-            "Yes", "No",
-        },
-    }
-    survey1.Questions = [] Question {
-        question1, question2
-    }
-    return survey1
+var survey1 Survey
+var survey2 Survey
+
+func init() {
+	study1 = Study{ID: 1, Title: "Sample Study", Description: "Just a dummy study", Surveys: []Survey{}}
+
+	survey1 = populateSurvey1()
+	survey2 = populateSurvey2()
+
+	study1.Surveys = []Survey{survey1, survey2}
+}
+
+func populateSurvey1() Survey {
+	survey := Survey{ID: 1, Title: "Sample Survey", Description: "Just a dummy survey", Questions: []Question{}, StudyId: 1}
+
+	question1 := Question{
+		ID:       1,
+		Text:     "Have you had any of these symptoms?",
+		SurveyId: survey.ID,
+		Answers: []string{
+			"Fever", "Chills", "Sneezing", "Cough",
+		},
+	}
+	question2 := Question{
+		ID:       2,
+		Text:     "Have you visited a doctor?",
+		SurveyId: survey.ID,
+		Answers: []string{
+			"Yes", "No",
+		},
+	}
+	survey.Questions = []Question{question1, question2}
+
+	return survey
+}
+
+func populateSurvey2() Survey {
+	survey := Survey{ID: 2, Title: "Another Sample Survey", Description: "Another dummy survey", Questions: []Question{}, StudyId: 1}
+
+	question1 := Question{
+		ID:       10,
+		Text:     "How often do you exercise?",
+		SurveyId: survey.ID,
+		Answers: []string{
+			"Never", "Once a week", "Twice a week", "Three times or more per week",
+		},
+	}
+	question2 := Question{
+		ID:       20,
+		Text:     "Did you get a flu shot this year?",
+		SurveyId: survey.ID,
+		Answers: []string{
+			"Yes", "No",
+		},
+	}
+	survey.Questions = []Question{question1, question2}
+
+	return survey
 }
 
 // Defining GraphQL types:
 
+var studyType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Study",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"description": &graphql.Field{
+				Type: graphql.String,
+			},
+			"surveys": &graphql.Field{
+				Type: graphql.NewList(surveyType),
+			},
+		},
+	},
+)
+
 var surveyType = graphql.NewObject(
-    graphql.ObjectConfig {
-        Name: "Survey",
-        Fields: graphql.Fields {
-            "id": & graphql.Field {
-                Type: graphql.Int,
-            },
-            "title": & graphql.Field {
-                Type: graphql.String,
-            },
-            "description": & graphql.Field {
-                Type: graphql.String,
-            },
-            "questions": & graphql.Field {
-                Type: graphql.NewList(questionType),
-            },
-        },
-    },
+	graphql.ObjectConfig{
+		Name: "Survey",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"description": &graphql.Field{
+				Type: graphql.String,
+			},
+			"questions": &graphql.Field{
+				Type: graphql.NewList(questionType),
+			},
+			"studyId": &graphql.Field{
+				Type: graphql.Int,
+			},
+		},
+	},
 )
 
 var questionType = graphql.NewObject(
-    graphql.ObjectConfig {
-        Name: "Question",
-        Fields: graphql.Fields {
-            "id": & graphql.Field {
-                Type: graphql.Int,
-            },
-            "text": & graphql.Field {
-                Type: graphql.String,
-            },
-            "surveyId": & graphql.Field {
-                Type: graphql.Int, // surveyType -> compiler will complain for existing loops when it comes to type-checking
-            },
-            "answers": & graphql.Field {
-                Type: graphql.NewList(graphql.String),
-            },
-        },
-    },
+	graphql.ObjectConfig{
+		Name: "Question",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"text": &graphql.Field{
+				Type: graphql.String,
+			},
+			"surveyId": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"answers": &graphql.Field{
+				Type: graphql.NewList(graphql.String),
+			},
+		},
+	},
 )
 
+var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootQuery",
+	Fields: graphql.Fields{
+		"surveysOfStudy": &graphql.Field{
+			Type:        graphql.NewList(surveyType),
+			Description: "Get all surveys of a specific study",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, ok := params.Args["id"].(int)
+				if ok {
+					return fetchSurveysOfStudy(id), nil
+				}
+				return nil, nil
+			},
+		},
+		"surveyById": &graphql.Field{
+			Type:        surveyType,
+			Description: "Get Survey By ID",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, ok := params.Args["id"].(int)
+				if ok {
+					return fetchSurveyById(id), nil
+				}
+				return nil, nil
+			},
+		},
+		"surveyQuestions": &graphql.Field{
+			Type:        graphql.NewList(questionType),
+			Description: "Get all questions of a specific survey",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, ok := params.Args["id"].(int)
+				if ok {
+					return fetchSurveyById(id).Questions, nil
+				}
+				return nil, nil
+			},
+		},
+	},
+})
+
+//todo: add mutation query
+
 func main() {
-    survey: = populate()
+	schemaConfig := graphql.SchemaConfig{Query: rootQuery}
+	schema, err := graphql.NewSchema(schemaConfig)
 
-    // Schema
-        fields: = graphql.Fields {
-        "surveyById": & graphql.Field {
-            Type: surveyType, // I assume this is the return type
-            Description: "Get Survey By ID",
-            Args: graphql.FieldConfigArgument {
-                "id": & graphql.ArgumentConfig {
-                    Type: graphql.Int,
-                },
-            },
-            Resolve: func(params graphql.ResolveParams)(interface {}, error) {
-                id, ok: = params.Args["id"].(int)
-                if ok {
-                    // Find the survey
-                    // There is only one survey for now
-                    if int(survey.ID) == id {
-                        return survey, nil
-                    }
-                }
-                return nil, nil
-            },
-        },
-        "surveyQuestions": & graphql.Field {
-            Type: graphql.NewList(questionType),
-            Description: "Get all questions of a survey",
-            Args: graphql.FieldConfigArgument {
-                "id": & graphql.ArgumentConfig {
-                    Type: graphql.Int,
-                },
-            },
-            Resolve: func(params graphql.ResolveParams)(interface {}, error) {
-                id, ok: = params.Args["id"].(int)
-                if ok {
-                    // First find the survey
-                    // There is only one survey for now
-                    if int(survey.ID) == id {
-                        return survey.Questions, nil
-                    }
-                }
-                return nil, nil
-            },
-        },
-    }
-    rootQuery: = graphql.ObjectConfig {
-        Name: "RootQuery",
-        Fields: fields
-    }
-    schemaConfig: = graphql.SchemaConfig {
-        Query: graphql.NewObject(rootQuery)
-    }
-    schema,
-    err: = graphql.NewSchema(schemaConfig)
-    if err != nil {
-        log.Fatalf("failed to create new schema, error: %v", err)
-    }
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
 
-    // Query
-    query: = `
-        {
-            surveyQuestions(id:1) {
-                text
-                surveyId
-                answers
-            }
-        }
-    `
-    params: = graphql.Params {
-        Schema: schema,
-        RequestString: query
-    }
-    r: = graphql.Do(params)
-    if len(r.Errors) > 0 {
-        log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
-    }
-    rJSON,
-    _: = json.Marshal(r)
-    fmt.Printf("%s \n", rJSON)
+	h := handler.New(&handler.Config{
+		Schema: &schema,
+		Pretty: true,
+	})
+
+	http.Handle("/graphql", h)
+	http.ListenAndServe(":8080", nil)
+
+    /* Query Sample:
+	  query := `
+	      {
+	          surveyQuestions(id:1) {
+	              text
+	              surveyId
+	              answers
+	          }
+	      }
+	  `*/
+}
+
+//Helper functions, later to be moved:
+//This part is mocking the data layer:
+func fetchSurveyById(id int) Survey {
+	if id == survey1.ID {
+		return survey1
+	}
+	if id == survey2.ID {
+		return survey2
+	}
+	return Survey{}
+}
+
+func fetchSurveysOfStudy(id int) []Survey {
+	//since there is only one study now
+	if id == 1 {
+		return study1.Surveys
+	}
+	return []Survey{}
 }
